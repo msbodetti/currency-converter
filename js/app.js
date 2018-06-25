b85e5b2bdfdb146dfa4268c6ae86fe6c9ed6c3ec
Yentl <yentl@web-designer.ninja>
2018-06-25 21:18:00 +0200

    Store currency info from API into database

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
 (function() {
     'use strict';
 
+    const apiURL = `https://free.currencyconverterapi.com/api/v5/countries`;   
+    let countriesCurrencies;
+    const dbPromise = idb.open('countries-currencies', 0, upgradeDB => {
+        // Note: we don't use 'break' in this switch statement,
+        // the fall-through behaviour is what we want.
+        switch (upgradeDB.oldVersion) {
+            case 0:
+            upgradeDB.createObjectStore('objs', {keyPath: 'id'});
+        }
+    });
+    fetch(apiURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(currencies) {
+        dbPromise.then(db => {
+            if(!db) return;
+            countriesCurrencies = [currencies.results];
+            const tx = db.transaction('objs', 'readwrite');
+            const store = tx.objectStore('objs');
+            let i = 0;
+            countriesCurrencies.forEach(function(currency) {
+                for (let value in currency) {
+                    store.put(currency[value]);
+                }
+            });
+            return tx.complete;
+        });
+    });
+
 })();
